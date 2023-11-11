d99326596248d1a4429ad4bf6326484706467299
Sameer Faridi <s.faridi007@gmail.com>
2023-11-11 12:40:30 +0530

    there it is

---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend"
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend"
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control"
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control"
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend"
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend"
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database"
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation"
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend"
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend"
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend"
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend"
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management"
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend"
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend"
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend"
+  }
+];
+
+export const experiences = [
+  {
+    title: "Front End Developer",
+    company_name: "4Archivers",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "Jul 2023 -  Now",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers."
+    ]
+  },
+  {
+    title: "Jr Software Developer",
+    company_name: "Abhiwan Technologies",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Mar 2023 - Jul 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers."
+    ]
+  },
+  {
+    title: "Technical Support Engineer",
+    company_name: "Dahua Technologies",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Oct 2022 - Mar 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers."
+    ]
+  }
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "https://wa.me/9639356395"
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/sameerkali"
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/sameer-faridi/"
+  }
+];
+
+export const projects = [
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Indie Gems Portal",
+    description: `Indie Gems Portal is a web application that allows users to search for
+      games based on genres, platforms, release dates, and ratings. It
+      provides a convenient way to explore and find games that match your
+      preferences.`,
+    link: "https://indie-gems-portal.vercel.app/"
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Film Sloth",
+    description: `🕵️‍♂️ FilmSloth: Unlock cinematic secrets! Discover hidden 🎥 gems
+      effortlessly and delve into the world of entertainment with our
+      intuitive app.`,
+    link: "https://filmsloth.netlify.app/"
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Explorer Mount",
+    description: `At Explore Mount, were on a mission to be your trusted companion on
+      every journey you embark upon`,
+    link: "https://explorer-mount.vercel.app/"
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "FitHub",
+    description: `FitHub is a straightforward and visually appealing landing page
+      specifically crafted for gyms and fitness centers. It offers a
+      pristine and efficient online representation,`,
+    link: "https://fithub-kappa.vercel.app/"
+  },
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "HOME HARBOR",
+    description: `HomeHarbor: Your Real Estate Solution HomeHarbor serves as a reliable
+      guide in the realm of real estate. Our platform offers a seamless
+      experience.`,
+    link: "https://home-harbour.onrender.com/"
+  },
+
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Portfolio for client",
+    description: `Welcome to my cutting-edge Personal Portfolio Project! 🚀 In This
+      project, Ive harnessed the power of modern technologies to create a
+      unique and immersive experience for showcasing my artistic endeavors.`,
+    link: "https://sameer69.netlify.app/"
+  }
+];
